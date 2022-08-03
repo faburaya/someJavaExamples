8a6cf9fd9202d8f6f433df4ba48ccbe36ed78107
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2022-08-04 01:29:31 +0200

    Implementiert die Klasse App.

--- JdbcTest/src/main/java/com/examples/jdbctest/App.java
+++ JdbcTest/src/main/java/com/examples/jdbctest/App.java
@@ -1,13 +1,110 @@
 package com.examples.jdbctest;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Clock;
+import java.util.Random;
+import java.util.logging.Logger;
+
+public class App {
+    private static final Logger logger;
+
+    static {
+        logger = Logger.getLogger(App.class.getCanonicalName());
+    }
+
+    private static final String TABLE_NAME = "Samples";
+
+    private static boolean createSchemaIfNotPresent(Connection dbConnection) throws SQLException {
+        DatabaseMetaData dbMetadata = dbConnection.getMetaData();
+        try (ResultSet tables = dbMetadata.getTables(null, null, null, new String[] { "TABLE" })) {
+            while (tables.next()) {
+                if (tables.getString("TABLE_NAME").equals(TABLE_NAME.toUpperCase())) {
+                    return false;
+                }
+            }
+        }
+        try (Statement statement = dbConnection.createStatement()) {
+            statement.executeUpdate(String.format("create table %s (id int, name varchar(16))", TABLE_NAME));
+        }
+        return true;
+    }
+
+    private static String generateRandomName(Random randomizer) {
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < 6; ++i) {
+            buffer.append((char) (randomizer.nextInt(0, 'Z' - 'A') + 'A'));
+        }
+        return buffer.toString();
+    }
+
+    private static void recordSomeData(Connection dbConnection) throws SQLException {
+        dbConnection.setAutoCommit(false);
+        try (PreparedStatement statement = dbConnection
+                .prepareStatement(String.format("insert into %s (id, name) values (?, ?)", TABLE_NAME))) {
+            Random randomizer = new Random();
+            randomizer.setSeed(Clock.systemUTC().millis());
+
+            final int rowCount = 3;
+            for (int i = 0; i < rowCount; ++i) {
+                statement.setInt(1, randomizer.nextInt());
+                statement.setString(2, generateRandomName(randomizer));
+                statement.addBatch();
+            }
+            statement.executeBatch();
+            dbConnection.commit();
+            logger.info(String.format("Der Datenbank wurden %d Zeilen hinzugefügt.", rowCount));
+        } catch (Exception ex) {
+            dbConnection.rollback();
+            throw ex;
+        } finally {
+            dbConnection.setAutoCommit(true);
+        }
+    }
+
+    private static void listData(Connection dbConnection) throws SQLException {
+        System.out.println("Alle Zeilen in der Datenbank:");
+        try (Statement statement = dbConnection.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(String.format("select id, name from %s", TABLE_NAME))) {
+                int i = 0;
+                while (resultSet.next()) {
+                    System.out.println(
+                            String.format("#%d: %d, %s", i, resultSet.getInt("id"), resultSet.getString("name")));
+                    ++i;
+                }
+            }
+        }
+    }
+
+    private static final Path DATABASE_FILE_PATH = Paths.get("temp", "app_database");
+    private static final Path CRED_FILE_PATH = Paths.get("temp", "credential.x");
+    private static final Path SECRET_FILE_PATH = Paths.get("temp", "secret.x");
+
+    public static void main(String[] args) {
+        try {
+            CredentialFetcherInterface credentialFetcher = new CredentialFetcher(CRED_FILE_PATH, SECRET_FILE_PATH,
+                    new InteractiveCredentialFetcher());
+
+            Credential credential = credentialFetcher.getCredential();
+
+            try (DerbyConnectionProvider dbConnectionProvider = new DerbyConnectionProvider(DATABASE_FILE_PATH,
+                    credential.getUserId(),
+                    credential.getPassword())) {
+                Connection dbConnection = dbConnectionProvider.getConnection();
+                boolean schemaNotPresent = createSchemaIfNotPresent(dbConnection);
+                logger.config(String.format("Schema der Datenbank schon vorhanden? %b", !schemaNotPresent));
+                recordSomeData(dbConnection);
+                listData(dbConnection);
+            }
+        } catch (Exception ex) {
+            StackTraceElement[] stackTrace = ex.getStackTrace();
+            logger.severe(String.format("%s\n@ %s", ex.toString(), stackTrace[stackTrace.length - 1].toString()));
+        }
     }
 }
